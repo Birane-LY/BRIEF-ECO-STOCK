fc56a29c73a4a5661e186c9ef375a537d075a02c
habii45 <habyl5430@gmail.com>
2026-07-23 09:57:36 +0000

    Configure CORS pour permettre la communication entre le frontend et l'API

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'architecture'
+    'architecture',
+    "corsheaders",
+    
 ]
 
 REST_FRAMEWORK = {
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
 ]
 
 ROOT_URLCONF = 'warehouse.urls'
@@ -119,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+     "http://localhost:3000",
+]
